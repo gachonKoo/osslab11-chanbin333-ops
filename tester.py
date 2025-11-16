18d7f0008a2ac85ea5c13b25af3eeb5ea958dbbc
chanbin333-ops <chanbin333@gachon.ac.kr>
2025-11-16 13:23:31 +0900

    fix: exact output '5.0 25.0' for autograder

--- tester.py
+++ tester.py
@@ -1,7 +1,7 @@
 from geo import distance, area_of_triangle
 
-print(distance(0, 0, 3, 4))
-print(area_of_triangle(10, 5))
+print("{:.1f} {:.1f}".format(distance(0, 0, 3, 4), area_of_triangle(10, 5)))
+
 
 
 
